59f703e6c598f4e139b00ccd4af0e49e425daa3c
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-26 01:58:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -92,7 +92,7 @@ end
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
-  alans_new_info ="GUI"
+  
 
 	programmer_hash = 
  		{
@@ -110,7 +110,7 @@ def changing_alan
       }
     }
     
-    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash[:alan_kay][:known_for] = "GUI"
 
 end
 
