6c0ceddea1c357186c685a888d053892d66c5b5a
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-26 02:07:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,8 +111,7 @@ def changing_alan
     }
     
     programmer_hash[:alan_kay][:known_for] = "GUI"
-    puts programmer_hash[:alan_kay][:known_for]
-
+    programmer_hash
 end
 
 def adding_to_dennis
