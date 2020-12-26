4074a6c1c4ca2b88f6bfd4a0e274ce6de594e922
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-26 01:56:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,7 +89,7 @@ def adding_matz
 
 end
 
-def changing_alan
+def changing_alan(alans_new_info)
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
 
